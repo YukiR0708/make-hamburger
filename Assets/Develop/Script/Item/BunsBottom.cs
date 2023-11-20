7819ba5cf3a148793cf4981ae8b52c044029e4df
結城 黎 <106291652+YukiR0708@users.noreply.github.com>
2023-11-21 04:31:00 +0900

    一次保存

---
+++ Assets/Develop/Script/Item/BunsBottom.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunsBottom : MonoBehaviour
+{
+    [SerializeField, Tooltip("積まれているアイテム")]
+    List<ItemData> _pileItem = new();
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Develop/Script/Item/StandardItem.cs
@@ -0,0 +1,19 @@
+//日本語対応
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
